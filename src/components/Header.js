5aed874fb872d46841b2ffe90c41527cab54f618
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-15 10:55:01 +0700

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,14 @@ import { useLocation } from "react-router-dom";
 
 const Header = ({ title, onAdd, showAdd }) => {
   const location = useLocation();
+
   return (
-    <header className="header">
-      <h1>{title}</h1>
+    <header className="flex justify-between items-center bg-indigo-600 text-white px-4 py-4 rounded-md shadow">
+      <h1 className="text-xl font-bold">{title}</h1>
       {location.pathname === "/" && (
         <Button
           onClick={onAdd}
-          color={showAdd ? "red" : "green"}
+          color={showAdd ? "#dc2626" : "#16a34a"} // red / green
           text={showAdd ? "Close" : "Add"}
         />
       )}
