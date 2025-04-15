90f047d329e0b393895e573cb59ea580042ba2bd
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-15 10:54:25 +0700

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,14 @@ import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>
-      <h4>Version 1.0.0</h4>
-      <Link to="/">Go Back</Link>
+    <div className="max-w-md mx-auto my-10 bg-white p-6 rounded-lg shadow text-center">
+      <h4 className="text-xl font-bold text-gray-700 mb-4">Version 1.0.0</h4>
+      <Link
+        to="/"
+        className="inline-block mt-2 px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700"
+      >
+        Go Back
+      </Link>
     </div>
   );
 };
