445d498e7928ee834ff4c51b9e7ea43df22fcd94
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-15 10:54:52 +0700

    Update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>Copyright &copy; 2021</p>
-      <Link to="/about">About</Link>
+    <footer className="mt-10 text-center text-sm text-gray-500 border-t pt-4">
+      <p>&copy; {new Date().getFullYear()} MyApp. All rights reserved.</p>
+      <Link to="/about" className="text-blue-500 hover:underline">
+        About
+      </Link>
     </footer>
   );
 };
