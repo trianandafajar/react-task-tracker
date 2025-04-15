0c4ac332d9fb0388505b55c8cf9d1eaaa610bbd8
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-15 10:55:18 +0700

    Update Tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,19 +1,21 @@
 import Task from "./Task";
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
+  if (tasks.length === 0) {
+    return <p className="text-center text-gray-500 mt-4">No Tasks To Show</p>;
+  }
+
   return (
-    <>
-      {tasks.length > 0
-        ? tasks.map((task) => (
-            <Task
-              key={task.id}
-              task={task}
-              onDelete={onDelete}
-              onToggle={onToggle}
-            />
-          ))
-        : "No Tasks To Show"}
-    </>
+    <div className="space-y-3 mt-4">
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
   );
 };
 
